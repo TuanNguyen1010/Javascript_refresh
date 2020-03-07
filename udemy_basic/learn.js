dcf3c0921a7956fdce5e2ad85d17e7ed45ac5a70
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-07 15:48:03 +0000

    Add: output functions added

--- udemy_basic/learn.js
+++ udemy_basic/learn.js
@@ -1,3 +1,8 @@
 function myfunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed"
 }
+
+function nofunction() {
+  document.getElementById("demo").innerHTML = "Hello World"
+}
+
