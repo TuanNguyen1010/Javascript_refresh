55d4714e4d7d5c78f3134c2e987527c1cd1b8fcc
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-08 21:26:18 +0000

    Chore: basic project completed with output

--- udemy_basic/Learn Basic JavaScript/01. Starter Files/js/main.js
+++ udemy_basic/Learn Basic JavaScript/01. Starter Files/js/main.js
@@ -1,7 +1,7 @@
-var noun = console.log("What's your favourite outdoor sport")
+var noun = prompt("What's your favourite outdoor sport")
 
-var verb = console.log("What's your favourite indoor sport")
+var verb = prompt("What's your favourite indoor sport")
 
-var name = console.log("Whose your favourite athelete")
+var name = prompt("Whose your favourite athelete")
 
-console.log("Do you like " + name + " to " + noun + " while " + verb + "?")
+document.write("Do you like " + name + " to " + noun + " while " + verb + "?")
