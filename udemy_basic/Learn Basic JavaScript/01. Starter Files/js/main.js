42d706e788f22a55e534a2c23d1e5fdb530e34a2
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-03-08 21:25:47 +0000

    Chore: basic project

---
+++ udemy_basic/Learn Basic JavaScript/01. Starter Files/js/main.js
@@ -0,0 +1,7 @@
+var noun = console.log("What's your favourite outdoor sport")
+
+var verb = console.log("What's your favourite indoor sport")
+
+var name = console.log("Whose your favourite athelete")
+
+console.log("Do you like " + name + " to " + noun + " while " + verb + "?")
